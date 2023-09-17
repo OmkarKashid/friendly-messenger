47cd0d36ddb73976adf3fcc173847c644f95dc3a
OmkarKashid <68742874+OmkarKashid@users.noreply.github.com>
2023-09-18 02:03:48 +0530

    update chats route

--- server/routes/chats.js
+++ server/routes/chats.js
@@ -1,15 +1,12 @@
-import express from "express";
-import path from "path";
+const express = require("express");
+const path = require("path");
 const chatsRouter = express.Router();
 
-import {fileURLToPath} from 'url';
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 chatsRouter.use(express.urlencoded({ extended: true }));
 
 chatsRouter.get("/", function (req, res, next) {
-    // res.sendFile(path.join(__dirname,"../public/chats.html"));
-    res.send("hi");
+  // res.sendFile(path.join(__dirname,"../public/chats.html"));
+  res.send("hi");
 });
 
-export default chatsRouter;
+module.exports = chatsRouter;
